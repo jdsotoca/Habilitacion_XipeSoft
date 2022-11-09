df22139a036ecd4589231d8a50a82f2c4bec3054
José Dario Soto <sotomark2021@gmail.com>
2022-11-09 16:30:20 -0500

    Subir Sprint Boot XipeSoft

---
+++ src/main/java/com/xipesoft/xipe/XipeApplication.java
@@ -0,0 +1,13 @@
+package com.xipesoft.xipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XipeApplication.class, args);
+	}
+
+}
