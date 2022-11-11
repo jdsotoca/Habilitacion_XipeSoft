9297b49984c9a2778f1396912cb17551e6a99974
José Dario Soto <sotomark2021@gmail.com>
2022-11-11 18:01:10 -0500

    Actualizar Archivos

--- src/main/java/com/xipesoft/xipe/controllers/UserController.java
+++ src/main/java/com/xipesoft/xipe/controllers/UserController.java
@@ -1,5 +1,37 @@
 package com.xipesoft.xipe.controllers;
 
-public class UserController {
-
+import com.xipesoft.xipe.entities.Users;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class UserController{
+    @RequestMapping(value="/user/{id}")
+    public Users getUsers(@PathVariable long id){
+        Users users = new Users();
+        users.setId(1);
+        users.setEmail("juan@gmail.com");
+        users.setTask("");
+        users.setProfile("");
+        users.setPassword("");
+        return users;
+    }
+    @RequestMapping(value="/user1")
+    public Users modifyUsers(){
+        Users users = new Users();
+        users.setEmail("juan@gmail.com");
+        users.setTask("");
+        users.setProfile("");
+        users.setPassword("");
+        return users;
+    }
+    @RequestMapping(value="/user2")
+    public Users deleteUsers(){
+        Users users = new Users();
+        users.setEmail("juan@gmail.com");
+        users.setTask("");
+        users.setProfile("");
+        users.setPassword("");
+        return users;
+    }
 }
---
+++ src/main/java/com/xipesoft/xipe/entities/Users.java
@@ -0,0 +1,49 @@
+package com.xipesoft.xipe.entities;
+
+public class Users {
+    private long id;
+    private String email;
+    private String task;
+    private String profile;
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
